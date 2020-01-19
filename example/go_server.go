089be910a09790bf7ebcb955e8a5b439396f3482
alessiosavi <alessiosavibtc@gmail.com>
2020-01-19 15:48:23 +0100

    v0.2.6
    - Use a global `http.Transport` instead of create a new one for each request(s)
    - Avoid to use the same number of `max number of open files` (ulimit), instead use the 70% of the onve available.
    - Avoid to defer during the parallel requests.
    - Performance request have to be sent to the go server (example folder)
    - Fix tests

---
+++ example/go_server.go
@@ -0,0 +1,27 @@
+// A go webserver for benchmark the request
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	counter int
+	addr    = flag.String("addr", ":9999", "TCP address to listen to")
+)
+
+func main() {
+	if err := fasthttp.ListenAndServe(*addr, requestHandler); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
+}
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	counter = counter + 1
+	_, _ = fmt.Fprintf(ctx, "Counter: "+strconv.Itoa(counter))
+}
